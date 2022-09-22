9831d216908c795f5d860fa86982a93c9f8bae71
castlea121 <adam.castle@gridsmart.co.uk>
2022-09-22 21:40:09 +0100

    Lesson 42

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,12 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  runApp(
+    MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: SafeArea(
-            child: Row(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Container(
-              color: Colors.white,
-              child: Text('CONT1'),
-            ),
-            Container(
-              color: Colors.red,
-              child: Text('CONT2'),
-            ),
-            Container(
-              color: Colors.pink,
-              child: Text('CONT3'),
-            ),
-          ],
-        )),
+        body: Container(),
       ),
-    );
-  }
+    ),
+  );
 }
